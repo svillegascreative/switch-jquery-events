ba565d31a8f6f1f4a94f508d5248ae924da4386c
sylvia villegas <sylvia@svillegas.com>
2017-02-15 14:31:49 -0500

    Store last changes in variables

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-  $('body').css('background-color', '#222');
-  $('h1').css('color', '#555');
+  var darkBody = $('body').css('background-color', '#222');
+  var darkText = $('h1').css('color', '#555');
 
 
 });
