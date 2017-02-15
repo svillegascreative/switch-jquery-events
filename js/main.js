781f82e174d7528ac8718fe8352c3a6ecbd07059
sylvia villegas <sylvia@svillegas.com>
2017-02-15 14:35:19 -0500

    Change text in h1

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
 
   var darkBody = $('body').css('background-color', '#222');
   var darkText = $('h1').css('color', '#555');
+  var darkText = $('h1').text('Hey, who turned off the lights?')
+
 
 
 });
