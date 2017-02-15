bdacff60ce00777817a2bb064775a1b8b40bec2b
sylvia villegas <sylvia@svillegas.com>
2017-02-15 14:36:28 -0500

    Switch class of button

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   var darkBody = $('body').css('background-color', '#222');
   var darkText = $('h1').css('color', '#555');
   var darkText = $('h1').text('Hey, who turned off the lights?')
+  $('button').attr('class', 'off')
+
 
 
 
